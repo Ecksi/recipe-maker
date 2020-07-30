6f5c8a03e94f5d00414d065e351fabc07978ab88
ecksi <jimecksi@gmail.com>
2020-07-30 09:03:29 -0600

    Add ability to add recipes to the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,10 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    recipes: [
+      {
+        title: 'Raspberry Icebox Cake',
+        instructions: 'Buy jello, cool-whip, and grahmn crackers. Put everything in a pot and then look up the actual recipe.'
+      },
+      {
+        title: 'Around the world oatmeal',
+        instructions: 'In a pot combine oatmeal, chocolate chunks, cayenne pepper, and milk. Over medium heat, cook for 10 minutes.'
+      }
+    ]
+  },
+  getters: {
+
   },
   mutations: {
+    addRecipe (state, recipe) {
+      state.recipes.push(recipe)
+    }
   },
   actions: {
+    addRecipe (context, recipe) {
+      context.commit('addRecipe', recipe)
+    }
   },
   modules: {
   }
