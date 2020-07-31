20f5ff13d736a26d52bf37658c609a6a11fb1ea9
ecksi <jimecksi@gmail.com>
2020-07-31 06:44:32 -0600

    Add view recipes component / route

--- src/components/atoms/Header.vue
+++ src/components/atoms/Header.vue
@@ -9,7 +9,7 @@ export default {
     <img src="https://via.placeholder.com/75" alt="placeholder img">
     <nav class="navigation">
       <router-link to="/add">Add Recipes</router-link>
-      <router-link to="/">View Recipes</router-link>
+      <router-link to="/view">View Recipes</router-link>
       <label>
         <input type="text" placeholder="Search">
       </label>
---
+++ src/views/ViewRecipes.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'ViewRecipes',
+</script>
+
+<template>
+  <div>
+    <h1>Look at all the food!</h1>
+    <ul v-for="(recipe, index) in currentRecipes" :key="index">
+      <li>{{ recipe.title }}</li>
+    </ul>
+  </div>
+</template>
